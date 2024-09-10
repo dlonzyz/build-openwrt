7e79d8b96c0c3f4f7b22953d6207b6d6cfa53b8b
dlonzyz <dlonzyz@gmail.com>
2024-09-11 06:58:15 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,7 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i 's%openwrt\/telephony.git%dlonzyz\/telephony.git%g' feeds.conf.default
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
